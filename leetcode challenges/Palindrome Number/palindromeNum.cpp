557be58781010666401db7e42c2d5bf216e646dd
Jackson Bentley <jacksonabentley@gmail.com>
2026-06-15 15:46:35 -0500

    Refactor palindrome check logic in isPalindrome
    Refactor palindrome check to avoid using pow() and improve readability.

--- leetcode challenges/Palindrome Number/palindromeNum.cpp
+++ leetcode challenges/Palindrome Number/palindromeNum.cpp
@@ -1,22 +1,27 @@
-// MUST #include <cmath>
 class Solution {
 public:
     bool isPalindrome(int x) {
-        // first check
         if(x < 0) return false;
 
         int numDigits = 0;
         int temp = x;
-
-        // get num digits
         while(temp > 0){
             numDigits += 1;
             temp /= 10;
         }
 
-        // returns if ! palindrome
-        for(int i=0; i<numDigits/2; i++){
-            if(x/static_cast<int>(pow(10, i)) % 10 != x/static_cast<int>(pow(10, numDigits-i-1)) % 10) return false;
+        for(int i = 0; i < numDigits/2; i++){
+            int firstDigit = x;
+            int lastDigit = x;
+            for(int j=0; j < i; j++){
+                firstDigit /= 10;
+            }
+            for(int j=0; j < numDigits - 1 - i; j++){
+                lastDigit /=10;
+            }
+            firstDigit %= 10;
+            lastDigit %= 10;
+            if(firstDigit != lastDigit) return false;
         }
 
         return true;
